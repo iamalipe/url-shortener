c3f2a9c99cfb9db5b99100d989edc04f68c27bf9
Abhiseck <Abhiseck@outlook.com>
2023-05-18 00:24:54 +0530

    v1.0.1 deployment fix 2

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import shortUrlModel from "./models/shortUrlModel";
